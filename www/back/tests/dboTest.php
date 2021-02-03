8f3002b253752c4f71eddf12e04d223079de16e7
sergeibux <sertux.dev@gmail.com>
2021-02-03 11:19:46 +0100

    back

--- www/back/tests/dboTest.php
+++ www/back/tests/dboTest.php
@@ -1,13 +1,18 @@
-<body>Dbo test...</body>
+<body>Dbo test... . . .  .  .  . <hr>
 <?php
 include_once '../Dbo.php';
 include_once '../Logger.php';
 function connectTest(){
         loggerLog('conexion to DB ... 0', 'connectTest()', 'dboLog.txt', true, false);
-        $dbo = new Dbo();
-        loggerLog('conexion to DB ... 1', 'connectTest()', 'dboLog.txt', true, false);
-        $dbo->connect();
-        loggerLog('conexion to DB ... 2', 'connectTest()', 'dboLog.txt', true, false);
+         $dbo = new Dbo();
+        loggerLog('conexion to DB ... 1', 'connectTest()', 'dboLog.txt', true, true);
+         $dbo->connect();
+        loggerLog('conexion to DB ... 2', 'connectTest()', 'dboLog.txt', true, true);
     }
     connectTest();
-    loggerLog('...', 'connectTest()', 'dboLog.txt', true, false);
+    loggerLog('...', 'connectTest()', 'dboLog.txt', true, true);
+
+?>
+<br>.
+<hr>
+</body>

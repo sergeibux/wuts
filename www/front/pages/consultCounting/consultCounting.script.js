fe2ffa955409858ccec08ee7c0fbf4dc81808d6a
sergeibux <sertux.dev@gmail.com>
2021-06-23 00:25:38 +0200

    API feature : list branches

--- www/front/pages/consultCounting/consultCounting.script.js
+++ www/front/pages/consultCounting/consultCounting.script.js
@@ -24,4 +24,45 @@ class ConsultCounting {
     getValue() {
         return Binder.scope[this.prop];
     }
+    
+    static async getBranchesNames(){
+    	var uri = '../../../back/API/species.php?branches=all';
+    	var branches = await this.getFromAPI(uri);
+    	var options = '';
+		branches.forEach((branch) => {
+			var name = branch.scientificTerms;
+			if (branch.frenchTerms != ""
+				&& branch.frenchTerms.toLowerCase() != "autres"){
+				name += " (" + branch.frenchTerms + ")";
+			}
+		  options += '<option class="mdc-text-field__input" value="' + name + '" />';
+		})
+
+		document.getElementById('branch').innerHTML = options;
+    }
+
+    
+    static async getFromAPI(uri){
+    	return new Promise(function (resolve, reject) {
+	    	var request = new XMLHttpRequest()
+	
+	    	request.open('GET', uri, true)
+	
+	    	request.onload = function () {
+	    		if (this.status >= 200 && this.status < 300) {
+		  		  	resolve(JSON.parse(this.response));
+	    		} else {
+	    			reject({
+	                    status: this.status,
+	                    statusText: xhr.statusText
+	                });
+	    		}
+	    	}
+	
+	    	request.send()
+    	});
+    }
+    
 }
+
+ConsultCounting.getBranchesNames();
